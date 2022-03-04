3c2c8fb6fcb1ebacf2189117a04694fe2edacadb
Suraj Jaiswal <suraj@agrahyah.com>
2022-03-05 02:17:18 +0530

    Arrow Function added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 import Header from "./Header"
 import Footer from "./Footer"
-import createNotes from "./Note"
 import notes from "../notes.js"
+import Note from "./Note"
 
 console.log(notes)
 
@@ -10,7 +10,15 @@ function App() {
     return <div>
         <Header></Header>
 
-        {notes.map(createNotes)}
+        {notes.map(note =>
+            <Note 
+            // key is used for removing warning
+            key={note.key}
+            title={note.title}
+            content={note.content}
+            />
+            )
+        }
 
         <Footer></Footer>
     </div>
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -9,15 +9,4 @@ function Note(props) {
     )
 }
 
-function createNotes(note) {
-    return (
-        <Note 
-        // key is used for removing warning
-        key={note.key}
-        title={note.title}
-        content={note.content}
-        />
-    )
-}
-
-export default createNotes;
+export default Note;
