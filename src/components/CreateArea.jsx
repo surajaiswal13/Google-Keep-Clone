4f329e01d81ddd718cdf2b87aa73b70235511c1b
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-03-14 02:03:38 +0530

    [UPDATE] Created Note State

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 function CreateArea() {
+
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  })
+
+  function handleChange(event) {
+    const {name, value} = event.target
+
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      }
+    })
+    // console.log(value, name)
+    // console.log(event.target.value)
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
+        <textarea name="content" onChange={handleChange} value={note.content} placeholder="Take a note..." rows="3" />
         <button>Add</button>
       </form>
     </div>
